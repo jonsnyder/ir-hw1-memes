c3ab6f137ef95582ff8b1121e823dd77dce3acf9
Jon Snyder <jon@contextoptional.com>
2012-02-24 20:48:25 -0700

    initial commit of meme app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Memes::Application.config.secret_token = '2e26b994cc814d3d896b6b503e7d65446d962e5071c3d9392208bbe5fcdfc4d0822163d3d8292d8cfe612abdd5e72e9e64d90c066226327f713c3dc4124610e7'
